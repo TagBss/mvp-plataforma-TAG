e1fd8ced252455316a311e5c5fa43a604c222a7b
igortagbss <igor.fonseca@tagbss.com>
2025-07-16 16:33:06 -0300

    adjustment url allow_origins on backend

--- backend/main.py
+++ backend/main.py
@@ -49,7 +49,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://dashboard-nextjs-and-fastapi.vercel.app",
+        "https://mvp-plataforma-tag.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
